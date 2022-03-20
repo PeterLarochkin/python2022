84d48502e9ccc0694849835988a191200b4ee42d
Peter <plarochkin@gmail.com>
2022-03-21 00:56:31 +0300

    add figdate_wrapper

---
+++ 02_PushPip/figdate_wrapper.py
@@ -0,0 +1,18 @@
+from venv import create
+from tempfile import mkdtemp
+from subprocess import run
+from shutil import rmtree
+import argparse
+
+
+if __name__ == "__main__":
+    temp_dir = mkdtemp()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-format", default="%Y %d %b, %A", type=str)
+    parser.add_argument("-font", default="graceful", type=str)
+    args = parser.parse_args()
+    print(temp_dir)
+    create(temp_dir, clear=True, with_pip=True)
+    run([temp_dir+"/bin/pip3", "install", "pyfiglet"])
+    run([temp_dir+"/bin/python3", "-m", "figdate", "-format", args.format, "-font", args.font])
+    rmtree(temp_dir)
